1dea6245ec5cae16f5c6a5eb788eb64b43d51201
ReshetovIsme <razervv@gmail.com>
2021-08-17 09:59:44 +0300

    Task 5 integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://usajb0yct1.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
 };
