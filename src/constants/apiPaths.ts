c0b1572a1df05a464f746001599097a18c930730
ReshetovIsme <razervv@gmail.com>
2021-09-14 21:38:47 +0300

    fix path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://og1tzdw0kk.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://og1tzdw0kk.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://usajb0yct1.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://wguce6f2l7.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://og1tzdw0kk.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://og1tzdw0kk.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
